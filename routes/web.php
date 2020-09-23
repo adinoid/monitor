da6ffd34d7c3339fdfee14ac6fae672dc13d8dee
adinoid <ali.fahruddin.046@gmail.com>
2020-09-23 21:18:19 +0700

    kembali normal

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 // Route::get('/', 'LoginController@index');
 Route::group(['middleware' => ['auth']], function () {
   Route::get('/', 'HomeController@index');
-
+  Route::get('/home', 'HomeController@index');
   Route::get('/server', 'ServerController@index');
   Route::get('/processor', 'ProcessorController@index');
   Route::get('/statusserver', 'StatusServerController@index');
@@ -32,6 +32,3 @@ Route::group(['middleware' => ['auth']], function () {
   Route::get('/monitor', 'MonitorController@index');
   Route::post('/page-visibility', 'MonitorController@updatePageVisibility');
 });
-Route::get('/home', 'HomeController@index');
-
-Route::get('/serveeer', 'ServerController@inddex');
