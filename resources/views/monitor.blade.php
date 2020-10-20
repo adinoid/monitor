d1bb5e1ce3dcac1ac67a7a5741de46c71e2a2079
adinoid <ali.fahruddin.046@gmail.com>
2020-10-20 22:10:32 +0700

    tampilan server

--- resources/views/monitor.blade.php
+++ resources/views/monitor.blade.php
@@ -1,61 +1,85 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts/main')
 
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('title', 'Monitoring')
 
-  <title>Live Server Monitor</title>
+@section('breadcumbs')
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Cek</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Cek</li>
+        </ol>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+</section>
+@endsection
 
-  <script src="{{ asset('js/app.js') }}" defer></script>
+@section('content')
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- Default box -->
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Cek Monitoring</h3>
 
-  <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-</head>
-<body>
-  <div id="app">
-    <header>
-      <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-        <div class="container">
-          <a class="navbar-brand" href="{{ url('/') }}">Live Server Monitor</a>
-        </div>
-      </nav>
-    </header>
-
-    <main class="py-4 container">
-      <div class="row">
-
-        @forelse ($hosts as $host)
-          <div class="col">
-            <div class="card" style="width: 18rem;">
-              <div class="card-body">
-                <h5 class="card-title">{{ $host->name }}</h5>
-                <h6 class="card-subtitle mb-2 text-muted" id="host-{{ $host->id }}">Last updated: {{ minValue($host->checks) }}</h6>
-                @forelse (onlyEnabled($host->checks) as $check)
-                <ul class="mt-3">
-                  <li id="check-{{ $check->id }}">
-                    {{ $check->type }}: <span class="{{ $check->type }} {{ numberTextClass($check->type, $check->status, $check->last_run_message) }}">{{ $check->last_run_message }}</span>
-                  </li>
-                </ul>
-                @empty
-                <p class="card-text">No checks yet</p>
-                @endforelse
-              </div>
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                <i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                <i class="fas fa-times"></i></button>
             </div>
           </div>
-        @empty
-          <p>No hosts yet</p>
-        @endforelse
+          <div class="card-body">
 
+
+            <main class="py-4 container">
+              <div class="row">
+        
+                @forelse ($hosts as $host)
+                  <div class="col">
+                    <div class="card" style="width: 18rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">{{ $host->name }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted" id="host-{{ $host->id }}"> Last updated: {{ minValue($host->checks) }}</h6>
+                        @forelse (onlyEnabled($host->checks) as $check)
+                        <ul class="mt-3">
+                          <li id="check-{{ $check->id }}">
+                            {{ $check->type }}: <span class="{{ $check->type }} {{ numberTextClass($check->type, $check->status, $check->last_run_message) }}">{{ $check->last_run_message }}</span>
+                          </li>
+                        </ul>
+                        @empty
+                        <p class="card-text">No checks yet</p>
+                        @endforelse
+                      </div>
+                    </div>
+                  </div>
+                @empty
+                  <p>No hosts yet</p>
+                @endforelse
+        
+              </div>
+            </main>
+           
+          </div>
+          <!-- /.card-body -->
+          {{-- <div class="card-footer">
+            Footer
+          </div> --}}
+          <!-- /.card-footer-->
+        </div>
+        <!-- /.card -->
       </div>
-    </main>
+
+
+    </div>
   </div>
-
-  <script src="{{ asset('js/index.js') }}" defer></script>
-
-</body>
-</html>
+</section>
+@endsection
