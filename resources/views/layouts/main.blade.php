369388b7ee753f37a5714825d0ab00b941f1855f
adinoid <ali.fahruddin.046@gmail.com>
2020-12-03 11:39:57 +0700

    update composer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   {{-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> --}}
-  <meta http-equiv="refresh" content="30" />
+  <meta http-equiv="refresh" content="20" />
   <title>@yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
