74c84ae607c58390dc6680ae34a198a6bb30c685
adinoid <ali.fahruddin.046@gmail.com>
2020-12-07 23:03:47 +0700

    refresh 60 detik

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   {{-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> --}}
-  <meta http-equiv="refresh" content="20" />
+  <meta http-equiv="refresh" content="60" />
   <title>@yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
